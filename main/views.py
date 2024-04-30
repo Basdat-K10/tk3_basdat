ace6c35a6b30d7bb45ae995779118469a8455589
Arya KD <arya.kdaniswara@gmail.com>
2024-04-30 22:36:20 +0700

    connect with local postgres

--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from utils.query import query
 
 # Create your views here.
 def show_main(request):
-    return render(request, "main.html")
+
+    result = query("SELECT * FROM ulasan")  # Modify the SQL query as needed
+    context = {'result': result} 
+    return render(request, "main.html", context)
