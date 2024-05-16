e87ba53ce9bb0f6fab89299416f864cd8fd6700e
Arya KD <arya.kdaniswara@gmail.com>
2024-05-16 19:52:25 +0700

    connect to localhost

--- tk3_basdat/settings.py
+++ tk3_basdat/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = "tk3_basdat.wsgi.application"
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DB_NAME", "defaultdb"),
-        "USER": os.getenv("DB_USERNAME", "avnadmin"),
-        "PASSWORD": os.getenv("DB_PASSWORD","AVNS_ivRXzozyV6jfpn-JVxu"),
-        "HOST": os.getenv("DB_HOST", "basdat-k10-arya-2952.g.aivencloud.com"),
-        "PORT": os.getenv("DB_PORT", "23774")
+        "NAME": os.getenv("DB_NAME", "postgres"),
+        "USER": os.getenv("DB_USERNAME", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD","password"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432")
     }
 }
 
