2f53635d0091c9a404cf163c85cb5bf0078335a7
Arya KD <arya.kdaniswara@gmail.com>
2024-05-17 02:08:45 +0700

    feat: halaman langganan with R function

--- langganan/urls.py
+++ langganan/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from .views import show_paket, show_beli
+from .views import show_paket, show_beli, show_user_paket
 
 urlpatterns = [
-    path('', show_paket, name='show_paket'),
+    path('', show_user_paket, name='show_user_paket'),
     path('pembelian', show_beli, name='show_beli'),
 ]
--- langganan/views.py
+++ langganan/views.py
@@ -1,9 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from utils.query import query
 # Create your views here.
 
 def show_user_paket(request):
-    context = {}
+    if "username" not in request.session:
+        print("Username not found in session")
+        return redirect('/main/login/')
+     
+    username = "SarahJohnson" # JANGAN LUPA DIGANTI
+    print('username')
+    user_paket = query("""
+        SELECT 
+            t.nama_paket,
+            t.start_date_time,
+            t.end_date_time,
+            t.metode_pembayaran,
+            t.timestamp_pembayaran,
+            p.harga,
+            p.resolusi_layar,
+            STRING_AGG(dp.dukungan_perangkat, ', ') AS dukungan_perangkat
+        FROM 
+            TRANSACTION t
+        JOIN 
+            PAKET p ON t.nama_paket = p.nama
+        JOIN 
+            dukungan_perangkat AS dp ON p.nama = dp.nama_paket 
+        WHERE 
+            t.username = %s
+        GROUP BY
+            t.nama_paket,
+            t.start_date_time,
+            t.end_date_time,
+            t.metode_pembayaran,
+            t.timestamp_pembayaran,
+            p.harga,
+            p.resolusi_layar
+        ORDER BY 
+            t.timestamp_pembayaran DESC 
+        LIMIT 1;       
+    """, [username])
+    
+    all_paket = query("""
+        SELECT
+            p.nama,
+            p.harga,
+            p.resolusi_layar,
+            STRING_AGG(dp.dukungan_perangkat, ', ') AS dukungan_perangkat
+        FROM
+            paket AS p
+        LEFT JOIN
+            dukungan_perangkat AS dp ON p.nama = dp.nama_paket
+        GROUP BY
+            p.nama, 
+            p.harga, 
+            p.resolusi_layar           
+    """)
+
+    transaction_history = query("""
+        SELECT
+            t.username,
+            t.start_date_time,
+            t.end_date_time,
+            t.nama_paket,
+            p.harga,
+            t.metode_pembayaran,
+            t.timestamp_pembayaran
+        FROM
+            TRANSACTION t
+        JOIN
+            PAKET p ON t.nama_paket = p.nama
+        WHERE
+            t.username = %s
+        ORDER BY
+            t.start_date_time DESC;
+        """, [username])
+
+
+    context = {"user_paket": user_paket[0] if user_paket else None, "all_paket":all_paket, "transaction_history":transaction_history}
+    return render(request, "langganan.html", context)
 
 def show_paket(request):
     context = {}
