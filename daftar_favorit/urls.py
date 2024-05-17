66e96ada7eab441d2cfcbd8bf6fce2f157f582a1
neinakada <neina.akada@ui.ac.id>
2024-05-17 15:59:45 +0700

    Update: daftar_favorit, daftar_unduh

--- daftar_favorit/urls.py
+++ daftar_favorit/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import (
-    show_daftar_favorit,
     show_daftar,
     list_daftar_favorit,
     delete_daftar_favorit,
@@ -13,6 +12,6 @@ urlpatterns = [
     path('all/', show_daftar, name='show_daftar'),
     path('daftar/', list_daftar_favorit, name='list_daftar_favorit'),
     path('daftar/delete/<str:timestamp>/<str:username>/', delete_daftar_favorit, name='delete_daftar_favorit'),
-    path('daftar_favorit/<str:timestamp>/<str:username>/', list_isi_daftar_favorit, name='list_isi_daftar_favorit'),
+    path('list_isi_daftar_favorit/', list_isi_daftar_favorit, name='list_isi_daftar_favorit'),
     path('daftar_favorit/delete/<uuid:id_tayangan>/<str:timestamp>/<str:username>/', delete_isi_daftar_favorit, name='delete_isi_daftar_favorit'),
 ]
--- daftar_unduh/views.py
+++ daftar_unduh/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from .models import Unduhan
+from django.shortcuts import render, redirect
+from utils.query import query
 
 def show_daftar_unduh(request):
-    daftar_unduh = Unduhan.objects.all()
+    return render(request, "daftar_unduh.html")
 
+# READ list daftar unduh
+def list_daftar_unduh(request):
+    logged_in_user = request.session["username"]
+    q = 'SELECT timestamp, username, judul FROM tayangan_terunduh WHERE username = %s'
+    daftar_unduh = query(q, [logged_in_user])
+    print(logged_in_user)
     return render(request, 'daftar_unduh.html', {'daftar_unduh': daftar_unduh})
+
+# DELETE list daftar unduh
+def delete_tayangan(timestamp, username):
+    q = 'DELETE FROM tayangan_terunduh WHERE timestamp = %s AND username = %s'
+    query(q, [timestamp, username])
+    return redirect('list_daftar_unduh')
