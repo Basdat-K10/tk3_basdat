e7d0961876c51fb8d767d042293cdf35eb2ae0be
neinakada <neina.akada@ui.ac.id>
2024-05-17 13:59:30 +0700

    add: daftar favorit

--- daftar_favorit/urls.py
+++ daftar_favorit/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path, include
-from .views import show_daftar_favorit, show_daftar
+from django.urls import path
+from .views import (
+    show_daftar_favorit,
+    show_daftar,
+    list_daftar_favorit,
+    delete_daftar_favorit,
+    list_isi_daftar_favorit,
+    delete_isi_daftar_favorit
+)
 
 urlpatterns = [
-    path('', show_daftar_favorit, name='show_daftar_favorit'),
+    path('', list_daftar_favorit, name='list_daftar_favorit'),
     path('all/', show_daftar, name='show_daftar'),
+    path('daftar/', list_daftar_favorit, name='list_daftar_favorit'),
+    path('daftar/delete/<str:timestamp>/<str:username>/', delete_daftar_favorit, name='delete_daftar_favorit'),
+    path('daftar_favorit/<str:timestamp>/<str:username>/', list_isi_daftar_favorit, name='list_isi_daftar_favorit'),
+    path('daftar_favorit/delete/<uuid:id_tayangan>/<str:timestamp>/<str:username>/', delete_isi_daftar_favorit, name='delete_isi_daftar_favorit'),
 ]
--- daftar_favorit/views.py
+++ daftar_favorit/views.py
@@ -1,12 +1,56 @@
-from django.shortcuts import render
-from daftar_favorit.models import Favorit, Daftar
+from django.shortcuts import render, redirect
+from django.db import connection
+from django.contrib.auth.decorators import login_required
+
+def execute_sql(query, params=None):
+    with connection.cursor() as cursor:
+        cursor.execute(query, params)
+        if query.strip().lower().startswith('select'):
+            return cursor.fetchall()
+        else:
+            return None
 
 def show_daftar_favorit(request):
-    daftar_favorit = Favorit.objects.all()
-
-    return render(request, 'daftar_favorit.html', {'daftar_favorit': daftar_favorit})
+    return render(request, "daftar_favorit.html")
 
 def show_daftar(request):
-    daftar = Daftar.objects.all()
+    return render(request, "daftar.html")
 
-    return render(request, 'daftar.html', {'daftar_favorit': daftar})
+# READ list daftar favorit
+def list_daftar_favorit(request):
+    logged_in_user = request.session["username"]
+    query = 'SELECT timestamp, username, judul FROM daftar_favorit WHERE username = %s'
+    daftar_favorit = execute_sql(query, [logged_in_user])
+    print(logged_in_user)
+    return render(request, 'daftar.html', {'daftar_favorit': daftar_favorit})
+
+# @login_required
+# def list_daftar_favorit(request):
+#     print(request.session.username)
+#     logged_in_user = request.user.username
+#     query = 'SELECT timestamp, username, judul FROM daftar_favorit WHERE username = %s'
+#     daftar_favorit = execute_sql(query, [logged_in_user])
+#     return render(request, 'daftar.html', {'daftar_favorit': daftar_favorit})
+
+# DELETE list daftar favorit
+def delete_daftar_favorit(request, timestamp, username):
+    query = 'DELETE FROM daftar_favorit WHERE timestamp = %s AND username = %s'
+    execute_sql(query, [timestamp, username])
+    return redirect('list_daftar_favorit')
+
+# READ isi daftar favorit (judul tayangan)
+def list_isi_daftar_favorit(request, timestamp, username):
+    query = '''
+    SELECT t.judul 
+    FROM tayangan t
+    JOIN tayangan_memiliki_daftar_favorit tf ON t.id = tf.id_tayangan
+    WHERE tf.timestamp = %s AND tf.username = %s
+    '''
+    isi_daftar_favorit = execute_sql(query, [timestamp, username])
+    return render(request, 'daftar_favorit.html', {'isi_daftar_favorit': isi_daftar_favorit})
+
+# DELETE isi daftar favorit
+def delete_isi_daftar_favorit(request, id_tayangan, timestamp, username):
+    query = 'DELETE FROM tayangan_memiliki_daftar_favorit WHERE id_tayangan = %s AND timestamp = %s AND username = %s'
+    execute_sql(query, [id_tayangan, timestamp, username])
+    return redirect('list_isi_daftar_favorit', timestamp=timestamp, username=username)
