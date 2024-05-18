f2fa926a6425eb9ec9fea9fe06fe1ae24d6fe250
neinakada <neina.akada@ui.ac.id>
2024-05-18 23:24:44 +0700

    fix: delete

--- daftar_unduh/urls.py
+++ daftar_unduh/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import list_daftar_unduh
+from .views import list_daftar_unduh, delete_tayangan
 
 urlpatterns = [
     path('', list_daftar_unduh, name='list_daftar_unduh'),
+    path('delete_tayangan/<str:id>/', delete_tayangan, name='delete_tayangan'),
 ]
--- daftar_unduh/views.py
+++ daftar_unduh/views.py
@@ -17,7 +17,8 @@ def list_daftar_unduh(request):
     return render(request, 'daftar_unduh.html', {'daftar_unduh': daftar_unduh})
 
 # DELETE list daftar unduh
-def delete_tayangan(timestamp, username):
-    q = 'DELETE FROM tayangan_terunduh WHERE timestamp = %s AND username = %s'
-    query(q, [timestamp, username])
-    return redirect('list_daftar_unduh')
+def delete_tayangan(request, id):
+    logged_in_user = request.session["username"]
+    q = 'DELETE FROM tayangan_terunduh WHERE id_tayangan = %s AND username = %s'
+    query(q, [id, logged_in_user])
+    return redirect(request, 'daftar_unduh.html', {'daftar_unduh':list_daftar_unduh})
